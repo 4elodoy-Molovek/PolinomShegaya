54db3551fdd0a97a706ab6b9084f03c946db7c7a
1drid1 <ivandor228@outlook.com>
2025-03-10 22:04:43 +0300

    tests for ExpressionAnalyzer and Parser declared

---
+++ vsproj/base_test/testParser.cpp
@@ -0,0 +1,12 @@
+#include "gtest.h"
+#include "Parser.h"
+
+TEST(Parser, parser_Can_Convert_String_To_Polynomial)
+{
+	ADD_FAILURE();
+}
+
+TEST(Parser, parser_Can_Convert_Polynomial_To_String)
+{
+	ADD_FAILURE();
+}
