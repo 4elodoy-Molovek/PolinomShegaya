8a8426aa742603ab2f9ac10adac7f569a27d26ec
4elodoy Molovek <4elodoy Molovek@4ELODOY-LAPTOP>
2025-03-29 13:11:02 +0300

    released: HashTableOne, LinearListTable, Parser

---
+++ vsproj/PolynomialCalculator/HashTableOne.cpp
@@ -0,0 +1,65 @@
+﻿#include "HashTableOne.h"
+#include <stdexcept>
+#include <functional>
+
+template<typename K, typename T>
+HashTableOne<K, T>::HashTableOne(size_t sz) : size(0), bufferSize(sz), sizeAllNonNullptr(0)
+{
+    table.resize(bufferSize);
+}
+
+template<typename K, typename T>
+HashTableOne<K, T>::~HashTableOne() {}
+
+template<typename K, typename T>
+void HashTableOne<K, T>::addElement(const K& key, const T& pol)
+{
+    size_t index = hashFunction(key);
+    for (auto& pair : table[index])
+    {
+        if (pair.first == key)
+        {
+            pair.second.data = pol;
+            pair.second.deleted = false;
+            return;
+        }
+    }
+    table[index].emplace_back(key, Node{ pol, false });
+    ++size;
+    ++sizeAllNonNullptr;
+}
+
+template<typename K, typename T>
+void HashTableOne<K, T>::deleteElement(const K& key)
+{
+    size_t index = hashFunction(key);
+    for (auto& pair : table[index])
+    {
+        if (pair.first == key && !pair.second.deleted)
+        {
+            pair.second.deleted = true;
+            --size;
+            return;
+        }
+    }
+}
+
+template<typename K, typename T>
+T& HashTableOne<K, T>::findElement(const K& key)
+{
+    size_t index = hashFunction(key);
+    for (auto& pair : table[index])
+    {
+        if (pair.first == key && !pair.second.deleted)
+        {
+            return pair.second.data;
+        }
+    }
+    throw std::runtime_error("Element not found");
+}
+
+template<typename K, typename T>
+size_t HashTableOne<K, T>::hashFunction(const K& key) const
+{
+    return std::hash<K>{}(key) % bufferSize;
+}
---
+++ vsproj/PolynomialCalculator/LinearListTable.cpp
@@ -0,0 +1,41 @@
+﻿#include "LinearListTable.h"
+#include <stdexcept>
+
+template<typename K, typename T>
+LinearListTable<K, T>::LinearListTable(size_t sz) {}
+
+template<typename K, typename T>
+LinearListTable<K, T>::~LinearListTable() {}
+
+template<typename K, typename T>
+void LinearListTable<K, T>::addElement(const K& key, const T& pol)
+{
+    ListTableString<K, T> newNode{ key, pol };
+    data.insertLast(newNode);
+}
+
+template<typename K, typename T>
+void LinearListTable<K, T>::deleteElement(const K& key)
+{
+    for (size_t i = 0; i < data.size(); ++i)
+    {
+        if (data[i].key == key)
+        {
+            data.remove(i);
+            return;
+        }
+    }
+}
+
+template<typename K, typename T>
+T& LinearListTable<K, T>::findElement(const K& key)
+{
+    for (size_t i = 0; i < data.size(); ++i)
+    {
+        if (data[i].key == key)
+        {
+            return data[i].value;
+        }
+    }
+    throw std::runtime_error("Element not found");
+}
