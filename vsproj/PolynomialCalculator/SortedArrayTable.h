8cb4d9365384b36627370967f589c01aac951afe
smallAbyss <eal1105@yandex.ru>
2025-04-07 00:04:23 +0300

    add HashTableTwo and SortedArrayTable

--- vsproj/PolynomialCalculator/SortedArrayTable.h
+++ vsproj/PolynomialCalculator/SortedArrayTable.h
@@ -1,24 +1,89 @@
 #pragma once
 #include "Table.h"
+#include <vector>
+
 
 template<typename K, typename T>
 class SortedArrayTable : public Table<K, T>
 {
 protected:
-	std::vector<T> data;
-	size_t size;
-	size_t bufferSize;
+	struct Node {
+		K name;
+		T data;
+	};
+	std::vector<Node> table;
+
+	size_t binarySearch(K& x, bool& suc)
+	{
+		size_t low = 1, high = table.size(), mid = 42;
+		while (low <= high) {
+			mid = (high + low) / 2;
+			if (table[mid - 1].name == x) {
+				suc = true;
+				return mid - 1;
+			}
+
+			if (table[mid - 1] < x)
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+		return low - 1; // потому что выход low за пределы high покажет место, на которое нужно поместить элемент
+		// в конц предпоследней итерации (последняя - когда low становиться = high) high "срезает" все числа между средним и предшествующим ему, 
+		// а число, которое мы ищем, оказывается между ними
+		// x = 4: [3, 5] -> [3], 5
+		// причина - предположение, что x = 4 есть в массиве, да оно должно быть на месте 3
+		// значит когда 3], [5 ```low = {5}, high = {3}``` low покажет ячейку, в которой при возрастающем массиве (счёт слева направо) должен быть x = 4 
+		// 
+		// крч x =4: 3, 5 - границы сходятся на ближайшем меньшем (в силу +/- 1 в изменении границ?) -> даём low выйти на еденрицу вперёд
+		// x=6: 5,7 - сходятся на ближайшем большем - low опять покажет истинное положение
+	}
 
 public:
-	SortedArrayTable(size_t sz = defaultSize);
-	~SortedArrayTable() override;
+	SortedArrayTable(size_t sz = defaultSize) {
+		table.resize(sz);
+	}
+	~SortedArrayTable() = default;
 
-	// Äîáàâëÿåò â òàáëèöó ýëåìåíò ñ êëþ÷åì key
-	virtual void addElement(const K& key, const T& pol) override;
+	// Добавляет полином pol в таблицу с ключем(именем) name
+	virtual void addElement(const K& name, const T& pol) {
+		bool suc = false;
+		size_t num = binarySearch(name, suc);
+		if (suc) { // ячейка с таким именем есть
+			return;
+		}
+		else {
+			table.push_back(table[table.size() - 1]);
+			for (size_t i = table.size() - 1; i > num; i--) {
+				table[i] = table[i - 1];
+			}
+			table[num] = { name, pol };
+		}
+	}
 
-	// Óäàëÿåò èç òàáëèöû ýëåìåíò ñ êëþ÷åì key
-	virtual void deleteElement(const K& key) override;
+	// Удаляет из полином с именем name из таблицы
+	virtual void deleteElement(const K& name) {
+		bool suc = false;
+		size_t num = binarySearch(name, suc);
+		if (!suc) { // ячейки с таким именем нет
+			return;
+		}
+		else {
+			for (size_t i = num; i < table.size() - 1; i++) {
+				table[i] = table[i + 1];
+			}
+			table.pop_back();
+		}
+	};
 
-	// Èùåò è âîçâðàùàåò ññûëêó íà ýëåìåíò ñ êëþ÷åì K, â ïðîòèâíîì ñëó÷àå áðîñàåò èñêëþ÷åíèå
-	virtual T& findElement(const K& key) override;
+	// Ищет в таблице полином с именем name
+	virtual T& findElement(const K& name) {
+		bool suc = false;
+		size_t num = binarySearch(name, suc) - 1;
+		if (!suc)
+			return nullptr;
+		else
+			return &table[num];
+	}
 };
+
